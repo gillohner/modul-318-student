bffb3f3c01ee5968dcae224d38db50bbffcb800e
gillohner <gil@lohner.net>
2019-05-01 16:25:51 +0200

    Weiterprogrammiert

--- TransportAPP_GUI/Form1.Designer.cs
+++ TransportAPP_GUI/Form1.Designer.cs
@@ -28,24 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox_Von = new System.Windows.Forms.ComboBox();
             this.lbl_Fahrplan = new System.Windows.Forms.Label();
             this.lbl_Von = new System.Windows.Forms.Label();
             this.lbl_Nach = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listbox_Von = new System.Windows.Forms.ListBox();
+            this.listbox_Nach = new System.Windows.Forms.ListBox();
+            this.txt_Von = new System.Windows.Forms.TextBox();
+            this.txt_Nach = new System.Windows.Forms.TextBox();
+            this.btn_Suchen = new System.Windows.Forms.Button();
+            this.listview_Ausgabe = new System.Windows.Forms.ListView();
+            this.columnAbfahrt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnAnkunft = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
-            // 
-            // comboBox_Von
-            // 
-            this.comboBox_Von.AllowDrop = true;
-            this.comboBox_Von.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.comboBox_Von.FormattingEnabled = true;
-            this.comboBox_Von.Location = new System.Drawing.Point(90, 178);
-            this.comboBox_Von.Name = "comboBox_Von";
-            this.comboBox_Von.Size = new System.Drawing.Size(258, 21);
-            this.comboBox_Von.TabIndex = 0;
-            this.comboBox_Von.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox_Von_KeyDown);
             // 
             // lbl_Fahrplan
             // 
@@ -62,7 +57,7 @@
             // 
             this.lbl_Von.AutoSize = true;
             this.lbl_Von.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Von.Location = new System.Drawing.Point(93, 151);
+            this.lbl_Von.Location = new System.Drawing.Point(101, 151);
             this.lbl_Von.Name = "lbl_Von";
             this.lbl_Von.Size = new System.Drawing.Size(45, 24);
             this.lbl_Von.TabIndex = 2;
@@ -78,24 +73,89 @@
             this.lbl_Nach.TabIndex = 3;
             this.lbl_Nach.Text = "Nach";
             // 
-            // comboBox1
+            // listbox_Von
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(479, 178);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(258, 21);
-            this.comboBox1.TabIndex = 4;
+            this.listbox_Von.FormattingEnabled = true;
+            this.listbox_Von.Location = new System.Drawing.Point(97, 201);
+            this.listbox_Von.Name = "listbox_Von";
+            this.listbox_Von.Size = new System.Drawing.Size(258, 121);
+            this.listbox_Von.TabIndex = 9;
+            this.listbox_Von.TabStop = false;
+            this.listbox_Von.DoubleClick += new System.EventHandler(this.listbox_Von_DoubleClick);
+            // 
+            // listbox_Nach
+            // 
+            this.listbox_Nach.FormattingEnabled = true;
+            this.listbox_Nach.Location = new System.Drawing.Point(479, 201);
+            this.listbox_Nach.Name = "listbox_Nach";
+            this.listbox_Nach.Size = new System.Drawing.Size(258, 121);
+            this.listbox_Nach.TabIndex = 10;
+            this.listbox_Nach.TabStop = false;
+            this.listbox_Nach.DoubleClick += new System.EventHandler(this.listbox_Nach_DoubleClick);
+            // 
+            // txt_Von
+            // 
+            this.txt_Von.Location = new System.Drawing.Point(97, 179);
+            this.txt_Von.Name = "txt_Von";
+            this.txt_Von.Size = new System.Drawing.Size(258, 20);
+            this.txt_Von.TabIndex = 0;
+            this.txt_Von.TextChanged += new System.EventHandler(this.txt_Von_TextChanged);
+            // 
+            // txt_Nach
+            // 
+            this.txt_Nach.Location = new System.Drawing.Point(479, 179);
+            this.txt_Nach.Name = "txt_Nach";
+            this.txt_Nach.Size = new System.Drawing.Size(258, 20);
+            this.txt_Nach.TabIndex = 1;
+            this.txt_Nach.TextChanged += new System.EventHandler(this.txt_Nach_TextChanged);
+            // 
+            // btn_Suchen
+            // 
+            this.btn_Suchen.BackColor = System.Drawing.Color.Red;
+            this.btn_Suchen.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Suchen.ForeColor = System.Drawing.Color.Snow;
+            this.btn_Suchen.Location = new System.Drawing.Point(536, 353);
+            this.btn_Suchen.Name = "btn_Suchen";
+            this.btn_Suchen.Size = new System.Drawing.Size(201, 56);
+            this.btn_Suchen.TabIndex = 2;
+            this.btn_Suchen.Text = "SUCHEN";
+            this.btn_Suchen.UseVisualStyleBackColor = false;
+            this.btn_Suchen.Click += new System.EventHandler(this.btn_Suchen_Click);
+            // 
+            // listview_Ausgabe
+            // 
+            this.listview_Ausgabe.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnAbfahrt,
+            this.columnAnkunft});
+            this.listview_Ausgabe.Location = new System.Drawing.Point(97, 468);
+            this.listview_Ausgabe.Name = "listview_Ausgabe";
+            this.listview_Ausgabe.Size = new System.Drawing.Size(640, 261);
+            this.listview_Ausgabe.TabIndex = 11;
+            this.listview_Ausgabe.UseCompatibleStateImageBehavior = false;
+            this.listview_Ausgabe.View = System.Windows.Forms.View.Details;
+            // 
+            // columnAbfahrt
+            // 
+            this.columnAbfahrt.Text = "Abfahrt";
+            // 
+            // columnAnkunft
+            // 
+            this.columnAnkunft.Text = "Ankunft";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(831, 805);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listview_Ausgabe);
+            this.Controls.Add(this.btn_Suchen);
+            this.Controls.Add(this.txt_Nach);
+            this.Controls.Add(this.txt_Von);
+            this.Controls.Add(this.listbox_Nach);
+            this.Controls.Add(this.listbox_Von);
             this.Controls.Add(this.lbl_Nach);
             this.Controls.Add(this.lbl_Von);
             this.Controls.Add(this.lbl_Fahrplan);
-            this.Controls.Add(this.comboBox_Von);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -104,13 +164,18 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox_Von;
         private System.Windows.Forms.Label lbl_Fahrplan;
         private System.Windows.Forms.Label lbl_Von;
         private System.Windows.Forms.Label lbl_Nach;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listbox_Von;
+        private System.Windows.Forms.ListBox listbox_Nach;
+        private System.Windows.Forms.TextBox txt_Von;
+        private System.Windows.Forms.TextBox txt_Nach;
+        private System.Windows.Forms.Button btn_Suchen;
+        private System.Windows.Forms.ListView listview_Ausgabe;
+        private System.Windows.Forms.ColumnHeader columnAbfahrt;
+        private System.Windows.Forms.ColumnHeader columnAnkunft;
     }
 }
 
--- TransportAPP_GUI/Form1.cs
+++ TransportAPP_GUI/Form1.cs
@@ -13,25 +13,85 @@ namespace TransportAPP_GUI
 {
     public partial class Form1 : Form
     {
+
+        //Membervariablen
+        Transport t = new Transport();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void AdresseSuchen(string Stationsname, ComboBox ComboBoxName)
+        private void StationSuchen(string Stationsname, ListBox ListBoxName)
         {
-            Transport t = new Transport();
-            Stations MyStations = t.GetStations(Stationsname);
+            Stations Stations = t.GetStations(Stationsname);
 
-            foreach (Station station in MyStations.StationList)
+            ListBoxName.Items.Clear();
+
+            foreach (Station station in Stations.StationList)
             {
-                ComboBoxName.Items.Add(station.Name);
+                try
+                { 
+                    ListBoxName.Items.Add(station.Name);
+                }
+                catch
+                {
+                }
             }
         }
-        private void comboBox_Von_KeyDown(object sender, KeyEventArgs e)
+
+        private void TextBoxBefuellung(ListBox ListBoxName, TextBox TextBoxName)
         {
-            comboBox_Von.DroppedDown = true;
-            AdresseSuchen(comboBox_Von.Text, comboBox_Von);
+            if (ListBoxName.SelectedIndex > -1)
+            {
+                TextBoxName.Text = ListBoxName.SelectedItems[0].ToString();
+                ListBoxName.Items.Clear();
+            }
+        }
+
+        private void Verbindung(ListView ListViewName)
+        {
+            Connections Verbindungen = t.GetConnections(txt_Von.Text, txt_Nach.Text);
+
+            foreach (Connection connection in Verbindungen.ConnectionList)
+            {
+                DateTime Departure = DateTime.Parse(connection.From.Departure);
+                DateTime Arrival = DateTime.Parse(connection.To.Arrival);
+                try
+                {
+                    ListViewName.Items.Add(Departure.ToShortTimeString());
+
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void txt_Von_TextChanged(object sender, EventArgs e)
+        {
+            StationSuchen(txt_Von.Text, listbox_Von);
+        }
+
+        private void txt_Nach_TextChanged(object sender, EventArgs e)
+        {
+            StationSuchen(txt_Nach.Text, listbox_Nach);
+        }
+
+        private void btn_Suchen_Click(object sender, EventArgs e)
+        {
+            Verbindung(listview_Ausgabe);
+        }
+
+        private void listbox_Von_DoubleClick(object sender, EventArgs e)
+        {
+            TextBoxBefuellung(listbox_Von, txt_Von);
+        }
+
+        private void listbox_Nach_DoubleClick(object sender, EventArgs e)
+        {
+            TextBoxBefuellung(listbox_Nach, txt_Nach);
         }
     }
 }
